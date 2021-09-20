8c4981266255002e8a86f1cfbff096ee9062e323
safaa <aytansafa@gmail.com>
2021-09-20 16:38:21 +0300

    TestExecution, Timeouts , JUnit Test Execution

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -1,9 +1,15 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.*;
+
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 class IndexControllerTest {
 
@@ -14,16 +20,86 @@ class IndexControllerTest {
         controller = new IndexController();
     }
 
+
+    @DisplayName("Test Proper View name is returned for index page")
     @Test
     void index() {
-        assertEquals("index",controller.index());
-        assertEquals("index",controller.index(),"Wrong view Returned");
+        assertEquals("index", controller.index());
+        assertEquals("index", controller.index(), "Wrong view Returned");
     }
 
     @Test
+    @DisplayName("Test Exception")
     void oupsHandler() {
-        assertTrue("notimplemented".equals(controller.oupsHandler()), () -> "This is some expensive"+
+
+        assertThrows(ValueNotFoundException.class, () ->
+        {
+            controller.oupsHandler();
+        });
+
+       /* assertTrue("notimplemented".equals(controller.oupsHandler()), () -> "This is some expensive"+
                 "Message to build"+
-                "for my tests");
+                "for my tests");*/
+    }
+
+    @Disabled
+    @Test
+    void tetsTimeOut() {
+        assertTimeout(Duration.ofMillis(100), () -> {
+            Thread.sleep(5000);
+            System.out.println("I got here");
+        });
+    }
+
+    @Disabled
+    @Test
+    void tetsTimeOutPrempt() {
+        assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
+            Thread.sleep(5000);
+
+            System.out.println("I got here");
+        });
+
+    }
+    @Test
+    void testAssumptionTrue(){
+
+        assumeTrue("GURU".equalsIgnoreCase(System.getenv("GURU_RUNTIME")));
+    }
+
+    @Test
+    void testAssumptionAssumptionIsTrue(){
+        assumeTrue("GURU".equalsIgnoreCase("GURU"));
+    }
+
+    @EnabledOnOs(OS.MAC)
+    @Test
+    void testMeOnMacOS(){
+
+    }
+    @EnabledOnOs(OS.WINDOWS)
+    @Test
+    void testMeOnWindows(){
+
+    }
+    @EnabledOnJre(JRE.JAVA_8)
+    @Test
+    void testMeOnJava8(){
+
+    }
+    @EnabledOnJre(JRE.JAVA_11)
+    @Test
+    void testMeOnJava11(){
+
+    }
+    @EnabledIfEnvironmentVariable(named = "USER",matches = "s.aytan")
+    @Test
+    void testIfUserJt(){
+
+    }
+    @EnabledIfEnvironmentVariable(named = "USER",matches = "s.aytan")
+    @Test
+    void testIfUserFred(){
+
     }
 }
